09c70f9f110b9f06a87686aad75684718d622661
Yahir Mendez <mendez.yahireli@gmail.com>
2024-09-11 13:19:01 +0200

    Inicio de dashboard

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,4 @@
+import { Open_Sans } from 'next/font/google';
+
+export const OpenSans = Open_Sans({ subsets: ['latin']});
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+import { OpenSans } from "./fonts/fonts";
+import "./globals.css";
+
+export default function RootLayout({children}:Readonly<{children:React.ReactNode}>){
+
+  return(
+    <html lang="es" className={`${OpenSans.className} antialiased dark:bg-gray-950`} >
+      <body>{children}</body>
+    </html>
+  )
+}
